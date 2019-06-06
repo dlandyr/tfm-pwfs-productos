fce55a2784a1cdb508a884000e9554b8f337c64f
Dennys <dennys.landy@hotmail.com>
2019-06-06 04:01:17 +0200

    add ProductSearchInputDto #5

---
+++ src/main/java/es/upm/miw/dtos/input/ProductSearchInputDto.java
@@ -0,0 +1,66 @@
+package es.upm.miw.dtos.input;
+
+import java.math.BigDecimal;
+
+public class ProductSearchInputDto {
+
+    private String description;
+
+    private Integer stock;
+
+    private BigDecimal minPrice;
+
+    private BigDecimal maxPrice;
+
+    public ProductSearchInputDto() {
+    }
+
+    public ProductSearchInputDto(String description, Integer stock, BigDecimal minPrice, BigDecimal maxPrice) {
+        this.description = description;
+        this.stock = stock;
+        this.minPrice = minPrice;
+        this.maxPrice = maxPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public BigDecimal getMinPrice() {
+        return minPrice;
+    }
+
+    public void setMinPrice(BigDecimal minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public BigDecimal getMaxPrice() {
+        return maxPrice;
+    }
+
+    public void setMaxPrice(BigDecimal maxPrice) {
+        this.maxPrice = maxPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductSearchInputDto{" +
+                "description='" + description + '\'' +
+                ", stock=" + stock +
+                ", minPrice=" + minPrice +
+                ", maxPrice=" + maxPrice +
+                '}';
+    }
+}
